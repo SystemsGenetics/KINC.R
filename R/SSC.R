1fc41a6b56e414339887acb8e0bad9759a64da92
Stephen Ficklin <spficklin@gmail.com>
2017-11-08 22:51:10 -0800

    Added ability to highlight edge list in graph

--- R/SSC.R
+++ R/SSC.R
@@ -1,5 +1,4 @@
 
-
 #' Perfoms heirarchical clustering of edges in a network based on their sample compositions.
 #'
 #' This function uses the dist function to calucate a distance and the
@@ -205,64 +204,6 @@ drawEdgeListHeatMap = function(edge_indexes, osa, net, fieldOrder) {
   drawNetHeatMap(sm, ce.tree, osa, fieldOrder)
 }
 
-#' Draws the graph of a set of edges in the network
-#'
-#' @param edge_indexes
-#'   The index of the edges in the network that comprise the module.
-#' @param osa
-#'   The sample annotation matrix. One column must contain the header 'Sample'
-#'   and the remaining colums correspond to an annotation type.  The rows
-#'   of the anntation columns should contain the annotations.
-#' @param net
-#'   A network data frame containing the KINC-produced network.  The loadNetwork
-#'   function imports a dataframe in the correct format for this function.
-#'
-#' @export
-graphEdgeList = function(edge_indexes, osa, net) {
-  edges = as.vector(unlist(t(net[edge_indexes, c('Source', 'Target')])))
-  g = graph(edges, directed = F)
-
-  # Don't show node labels if we have too many nodes.
-  vlc = 0.5
-  vs = 10
-  if (length(edge_indexes) > 100) {
-    vlc = 0.25
-    vs = 5
-  }
-  if (length(edge_indexes) > 1000) {
-    vlc = 0.001
-    vs = 2
-  }
-
-  # Plot the graph.
-  l = layout_with_kk(g)
-  plot(g,
-       # Verticies
-       vertex.label.color="black", vertex.label.cex = vlc, vertex.size = vs,
-       # Edges
-       edge.color="#AAAAAA", vertex.color='cyan',
-       # Layout
-       layout = l)
-  return(list(
-    graph = g,
-    layout = l
-  ))
-}
-#' Draws the graph of the entire network
-#'
-#' @param net
-#'   A network data frame containing the KINC-produced network.  The loadNetwork
-#'   function imports a dataframe in the correct format for this function.
-#' @param osa
-#'   The sample annotation matrix. One column must contain the header 'Sample'
-#'   and the remaining colums correspond to an annotation type.  The rows
-#'   of the anntation columns should contain the annotations.
-#' @export
-graphNet = function(net, osa) {
-  edge_indexes = c(1:length(net$Source));
-  g = graphEdgeList(edge_indexes, osa, net)
-  return(g)
-}
 #' Performs enrichment analysis of traits against a a single edge in the network.
 #'
 #' @param i
--- R/base.R
+++ R/base.R
@@ -49,4 +49,95 @@ loadSampleAnnotations = function (annotation_file, sample_order) {
 
   return(osa)
 }
+#' Draws the graph of the entire network
+#'
+#' @param net
+#'   A network data frame containing the KINC-produced network.  The loadNetwork
+#'   function imports a dataframe in the correct format for this function.
+#' @param osa
+#'   The sample annotation matrix. One column must contain the header 'Sample'
+#'   and the remaining colums correspond to an annotation type.  The rows
+#'   of the anntation columns should contain the annotations.
+#' @export
+graphNet = function(net, osa) {
+  edge_indexes = c(1:length(net$Source));
+  g = graphEdgeList(edge_indexes, osa, net)
+  return(g)
+}
 
+#' Draws the graph of a set of edges in the network
+#'
+#' @param edge_indexes
+#'   The index of the edges in the network that comprise the module.
+#' @param osa
+#'   The sample annotation matrix. One column must contain the header 'Sample'
+#'   and the remaining colums correspond to an annotation type.  The rows
+#'   of the anntation columns should contain the annotations.
+#' @param net
+#'   A network data frame containing the KINC-produced network.  The loadNetwork
+#'   function imports a dataframe in the correct format for this function.
+#'
+#' @export
+graphEdgeList = function(edge_indexes, osa, net) {
+  edges = as.vector(unlist(t(net[edge_indexes, c('Source', 'Target')])))
+  g = graph(edges, directed = F)
+
+  # Don't show node labels if we have too many nodes.
+  vlc = 1
+  vs = 10
+  if (length(edge_indexes) > 100) {
+    vlc = 0.25
+    vs = 5
+  }
+  if (length(edge_indexes) > 1000) {
+    vlc = 0.001
+    vs = 2
+  }
+
+  # Plot the graph.
+  l = layout_with_kk(g)
+  plot(g,
+       # Verticies
+       vertex.label.color="black", vertex.label.cex = vlc, vertex.size = vs,
+       # Edges
+       edge.color="#AAAAAA", vertex.color='cyan',
+       # Layout
+       layout = l)
+  return(list(
+    graph = g,
+    layout = l
+  ))
+}
+
+#' Colors the edges of a given edge list on the currentlly plotted graph.
+#'
+#'
+#' @param edge_indexes
+#'   The index of the edges in the network that comprise the module.
+#' @param graph
+#'   The igraph object as returned from graphNet or graphEdgeList.
+#' @param layout
+#'   The layout object as returned from graphNet or graphEdgeList
+#' @param net
+#'   A network data frame containing the KINC-produced network.  The loadNetwork
+#'   function imports a dataframe in the correct format for this function.
+#' @param color
+#'   The color that should be used to highlith the edges
+#'
+#' @export
+highlightEdgeList = function(edge_indexes, graph, layout, net, color) {
+   edges = net[edge_indexes, c('Source', 'Target')]
+   verts = names(V(graph))
+   layout = norm_coords(layout)
+   for (i in 1:nrow(edges)) {
+     source = edges[i, c('Source')]
+     target = edges[i, c('Target')]
+     sindex = which(verts == source)
+     tindex = which(verts == target)
+     x0 = layout[sindex, 1]
+     y0 = layout[sindex, 2]
+     x1 = layout[tindex, 1]
+     y1 = layout[tindex, 2]
+     segments(x0, y0, x1, y1, col = color)
+   }
+}
