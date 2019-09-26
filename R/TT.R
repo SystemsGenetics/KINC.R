09698ccf7b37ecb15972645ebd51cd1433dbb55e
JohnHadish <john.hadish@wsu.edu>
2019-09-26 14:14:46 -0700

    Added a fold change function

--- R/TT.R
+++ R/TT.R
@@ -100,3 +100,118 @@ TtestNetCat = function(net, ematrix, progressBar = TRUE){
   
   return(net2);
 }
+
+#' Fold Change Edge
+#' 
+#' Determines the fold change between the in and out group of each edge. 
+#' Returns 0 if there is not enough power to do test
+#' 
+#' Determines fold change through determining the abs(mean) of the in and out group, and dividing
+#' the larger abs(mean) by the smaller one.
+#' 
+#' 
+#' @param i
+#'   The index of the edge in the network
+#' @param net
+#'   A network data frame containing the KINC-produced network.  The loadNetwork
+#'   function imports a dataframe in the correct format for this function.
+#'   This function is intended to be run on a network that has already been
+#'   reduced by analyzeNetCat.
+#' @param ematrix
+#'   The expression matrix data frame.
+#'   
+#' @export
+FoldChangeEdgeCat = function(i, net, ematrix){
+  
+  # Gets the name of the Target and the Source gene
+  Source = net[i,]$Source
+  Target = net[i,]$Target
+  pvals = c(0,0) # Assume that no differences in means
+  names(pvals) = c("FoldChange_Source","FoldChange_Target")
+  k = 1
+  
+  # Convert the sample string to a numerical vector.
+  cluster_indexes = getEdgeSamples(i, net)
+  non_cluster_indexes = getNonEdgeSamples(i, net)
+  
+  min_samples = 2 # This parameter needs to be better thought out I think
+  # It is possible that we could make it so the sample sizes must be kind of equal 
+  # for it to allow the t-test to happen?
+  
+  # First check to make sure that there are at least 2
+  # samples in each cluster, then Perform fold change for each gene
+  if(length(cluster_indexes) >= min_samples & length(non_cluster_indexes) >= min_samples)
+  {
+    for(j in c(Source, Target))
+    {
+      # Test to see if gene breaks the null hypothesis
+      in_mean = abs(mean(as.numeric(ematrix[j, cluster_indexes]), na.rm=TRUE))
+      out_mean = abs(mean(as.numeric(ematrix[j, non_cluster_indexes]), na.rm=TRUE))
+      if(in_mean >= out_mean)
+      {
+        foldChange = in_mean / out_mean
+      }
+      else
+      {
+        foldChange = out_mean / in_mean
+      }
+      pvals[k] = foldChange
+      k = k + 1
+    }
+  }
+  
+  # return the p.values
+  return(pvals);
+}
+
+
+
+
+#' FoldChange Net
+#' 
+#' Determines fold change through determining the abs(mean) of the in and out group, and dividing
+#' the larger abs(mean) by the smaller one.
+#' 
+#' Does this for your network of interest
+#' 
+#' 
+#' @param net
+#'   A network data frame containing the KINC-produced network.  The loadNetwork
+#'   function imports a dataframe in the correct format for this function.
+#'   This function is intended to be run on a network that has already been
+#'   reduced by analyzeNetCat.
+#' @param ematrix
+#'   The expression matrix data frame.
+#' @param progressBar
+#'   Set to FALSE to repress progress bar
+#' 
+#' @export
+FoldChangeNetCat = function(net, ematrix, progressBar = TRUE){
+  # Add in new columns for the Ttest
+  net2 = net
+  net2["FoldChange_Source"] = NA
+  net2["FoldChange_Target"] = NA
+  
+  # Intialize the progress bar
+  if (progressBar){
+    pb <- txtProgressBar(min = 0, max = nrow(net), style = 3)
+  }
+  
+  # Iterate through the rows of the network
+  for (i in 1:nrow(net)) {
+    if (progressBar){
+      setTxtProgressBar(pb, i)
+    }
+
+    FoldChange = FoldChangeEdgeCat(i, net, ematrix)
+    
+    #print(p.value)
+    net2[i, "FoldChange_Source"] = FoldChange["FoldChange_Source"]
+    net2[i, "FoldChange_Target"] = FoldChange["FoldChange_Target"]
+  }
+  if (progressBar){
+    close(pb)
+  }
+  
+  return(net2);
+}
